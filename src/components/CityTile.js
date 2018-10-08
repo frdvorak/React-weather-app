791343f233c4a339f4bb1aaa426a8e99806d008f
frdvorak <frantisek.dvorak011@gmail.com>
2018-10-08 20:56:01 +0100

    favicon, dynamic css for city silhouettes

---
+++ src/components/CityTile.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const CityTile = (props) => {
+       
+        return ( 
+            <span className="cityTile col-xs-12 col-sm-3 col-md-2 col-lg-2 col-xl-1">
+                <span className="inner" onClick={() => props.tileClick(props.city, props.country)} style={{backgroundImage: `url(./images/${props.name}.svg)`}}>
+                    <h5>{props.name}</h5>
+                </span>
+            </span>
+         );
+}
+
+ 
+export default CityTile;
---
+++ src/components/Titles.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Weather = () => {
+    return (
+        <div>
+                <h1>Current Weather</h1>
+                <p>Enter city and country or select from on of the cities below to find out current weather conditions.</p>
+        </div>
+    )
+}
+
+export default Weather;
