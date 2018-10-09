257a11caf62692e35b77d5ab2c643c6d766c9fb9
frdvorak <frantisek.dvorak011@gmail.com>
2018-10-09 21:42:47 +0100

    changed img path

--- src/components/CityTile.js
+++ src/components/CityTile.js
@@ -4,7 +4,7 @@ const CityTile = (props) => {
        
         return ( 
             <span className="cityTile col-xs-12 col-sm-6 col-md-4 col-lg-2 col-xl-1">
-                <span className="inner" onClick={() => props.tileClick(props.city, props.country)} style={{backgroundImage: `url(./images/${props.name}.svg)`}}>
+                <span className="inner" onClick={() => props.tileClick(props.city, props.country)} style={{backgroundImage: `url(images/${props.name}.svg)`}}>
                     <h5>{props.name}</h5>
                 </span>
             </span>
