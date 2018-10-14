59b5bd15259fa35f6435d9eec2d7e813210dff0f
frdvorak <frantisek.dvorak011@gmail.com>
2018-10-14 13:31:53 +0100

    weather-icon component - 6 weather scenarios implemented

--- src/components/WeatherIcon.js
+++ src/components/WeatherIcon.js
@@ -3,11 +3,18 @@ import React from 'react';
 const WeatherIcon = (props) => {
         console.log(props.description);
         let icon;
-        if (props.description == 'ligaht rain'){
+        if (props.description == 'light rain'){
+            icon = 'weather-rain';
+        } else if  (props.description == 'light rain'){
             icon = 'weather-sun-rain';
-            
-        } else {
+        } else if  (props.description == 'clear sky'){
             icon = 'weather-sun';
+        } else if  ((props.description == 'few clouds')||(props.description == 'broken clouds')){
+            icon = 'weather-sun-cloud';
+        } else if  (props.description == 'overcast clouds'){
+            icon = 'weather-clouds';
+        } else {
+            icon = 'weather-sun-rain';
         }
         
         let path = '../images/' + icon + '.svg';
@@ -20,5 +27,4 @@ const WeatherIcon = (props) => {
          );
 }
 
- 
 export default WeatherIcon;
