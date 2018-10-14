b84e71176ee14556b3f80a642b59ec7fba9d2ff9
frdvorak <frantisek.dvorak011@gmail.com>
2018-10-14 13:47:23 +0100

    weather-icon component - fully implemented

--- src/App.js
+++ src/App.js
@@ -154,9 +154,10 @@ class App extends Component {
           <CityTile name={'Venice'} city={'venice'} country={'it'} imageFile={'venice'} tileClick={this.tileClick}/>
           <CityTile name={'Washington'} city={'washington'} country={'us'} imageFile={'washington'} tileClick={this.tileClick}/>
           <CityTile name={'Amsterdam'} city={'amsterdam'} country={'nl'} imageFile={'amsterdam'} tileClick={this.tileClick}/>
+          <div>Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank" rel="noopener noreferrer">CC 3.0 BY</a></div>
+      
         </div>
-        <div>Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank" rel="noopener noreferrer">CC 3.0 BY</a></div>
-      </div>
+        </div>
     );
   }
 }
