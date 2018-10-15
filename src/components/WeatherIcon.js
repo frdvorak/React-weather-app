dc5eccaf73d2937ad686b02838dae8c13edccfaf
frdvorak <frantisek.dvorak011@gmail.com>
2018-10-15 19:40:03 +0100

    added more weather scenarios

--- src/components/WeatherIcon.js
+++ src/components/WeatherIcon.js
@@ -3,13 +3,13 @@ import React from 'react';
 const WeatherIcon = (props) => {
         //console.log(props.description);
         let icon;
-        if ((props.description === 'light rain')||(props.description === 'moderate rain')){
+        if ((props.description === 'light rain')||(props.description === 'moderate rain')||(props.description === 'light intensity drizzle rain')||(props.description === 'shower rain')){
             icon = 'weather-rain';
-        } else if  (props.description === 'heavy rain'){
+        } else if  ((props.description === 'heavy intensity rain')||(props.description === 'heavy intensity shower rain')){
             icon = 'weather-heavy-rain';
         } else if  (props.description === 'clear sky'){
             icon = 'weather-sun';
-        } else if  ((props.description === 'few clouds')||(props.description === 'broken clouds')){
+        } else if  ((props.description === 'few clouds')||(props.description === 'broken clouds')||(props.description === 'scattered clouds')){
             icon = 'weather-sun-cloud';
         } else if  (props.description === 'overcast clouds'){
             icon = 'weather-clouds';
@@ -17,7 +17,7 @@ const WeatherIcon = (props) => {
             icon = 'weather-storm';
         } else if  (props.description === 'heavy storm'){
             icon = 'weather-heavy-storm';
-        } else if  (props.description === 'fog'){
+        } else if  ((props.description === 'fog')||(props.description === 'mist')){
             icon = 'weather-fog';
         } else if  (props.description === 'snow'){
             icon = 'weather-snow';
